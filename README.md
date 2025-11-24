cb9b3f865a8ff740ffd4feb515c417e735c09583
kogure shinpei <z01035265@gmail.com>
2025-11-24 15:26:15 +0900

    テーブル作成ができたので、次はどこか一つのページを選んで、そこのphpを用いたコーディングを行います

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # product-1
-for internships
+DB情報の更新(init.sqlの更新)
+docker compose exec db sh
+mysql -u root -proot posse < /docker-entrypoint-initdb.d/init.sql
+
+mysql -u root -proot posse
+use posse;
+show tables;
+select * from テーブル名;
--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,3 +1,30 @@
 DROP DATABASE IF EXISTS posse;
 CREATE DATABASE posse;
 USE posse;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE diary (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT,
+    learned TEXT,
+    concern TEXT,
+    kind TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE advice (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    advice_text TEXT
+);
+
+CREATE TABLE todos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task VARCHAR(255) NOT NULL,
+    is_completed BOOLEAN DEFAULT FALSE
+);
